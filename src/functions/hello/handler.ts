a924622ffef716d2625cac51438a9bb8f32650e0
infantito <danielinfantito61@gmail.com>
2021-01-08 22:50:31 -0500

    build: ➕  serverless-offline

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -11,7 +11,6 @@ const hello: ValidatedEventAPIGatewayProxyEvent<
 > = async event => {
   return formatJSONResponse({
     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
   })
 }
 
